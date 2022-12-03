970bcdca5752193c3860358b1cb9d2993b679e6b
Brustik <minusminusov@gmail.com>
2022-12-03 22:11:05 +0300

    Переменные ДЗ

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+
+    public static void main(String[] args) {
+      var dog = 8.0;
+      var cat = 3.6;
+      var paper = 763789;
+
+      System.out.println(dog);
+      System.out.println(cat);
+      System.out.println(paper);
+
+      dog = dog + 4;
+      cat = cat + 4;
+      paper = paper + 4;
+      System.out.println(dog);
+      System.out.println(cat);
+      System.out.println(paper);
+
+      dog = dog - 3.5;
+      cat = cat - 1.6;
+      paper = paper - 7639;
+      System.out.println(dog);
+      System.out.println(cat);
+      System.out.println(paper);
+
+      var friend = 19;
+      friend = friend + 2;
+      friend = friend / 3;
+      System.out.println(friend);
+
+      var frog = 3.5;
+      System.out.println(frog);
+      frog = frog * 10;
+      System.out.println(frog);
+      frog = frog / 3.5;
+      System.out.println(frog);
+      frog = frog + 4;
+      System.out.println(frog);
+
+      //Задание 6
+
+
+
+    }
+}
